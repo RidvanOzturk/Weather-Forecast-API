c1016693c3e9903aad699c57be37d51600643ac3
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-11 13:50:22 +0300

    Added Fetch

--- WeatherForecastApi/src/App.jsx
+++ WeatherForecastApi/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Component = () => {
+  const [data, setData] = useState();
 
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    // fetch data
+    const dataFetch = async () => {
+      const data = await (
+        await fetch(
+          "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+        )
+      ).json();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      // set state when the data received
+      setData(data);
+    };
+
+    dataFetch();
+  }, []);
+
+  return<Component />;
 }
-
-export default App
