d3b79533f310101dfd8e4a54d95a92b1cecb1f5e
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-11 20:54:54 +0300

    tried some codes

--- WeatherForecastApi/src/App.jsx
+++ WeatherForecastApi/src/App.jsx
@@ -1,21 +1,39 @@
-const Component = () => {
-  const [data, setData] = useState();
+import React from "react";
+import './App.css';
+class App extends React.Component {
 
-  useEffect(() => {
-    // fetch data
-    const dataFetch = async () => {
-      const data = await (
-        await fetch(
-          "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
-        )
-      ).json();
+	constructor(props) {
+		super(props);
 
-      // set state when the data received
-      setData(data);
-    };
+		this.state = {
+			items: [],
+			DataisLoaded: false
+		};
+	}
 
-    dataFetch();
-  }, []);
 
-  return<Component />;
+	componentDidMount() {
+		fetch(
+"https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true
+				});
+			})
+	}
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Pleses wait some time.... </h1> </div> ;
+
+		return (
+		<div className = "App">
+			<h1> Fetch data from an api in react </h1> 
+		</div>
+	);
 }
+}
+
+export default App;
