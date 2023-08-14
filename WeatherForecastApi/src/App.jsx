4150a4a64cbfa46b6740ef7e535a330ce3b49eed
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-14 12:27:43 +0300

    edit some codes

--- WeatherForecastApi/src/App.jsx
+++ WeatherForecastApi/src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react";
 import './App.css';
 import Search from "./components/search/search";
 
 function App() {
+	const handleOnSearchChange = (searchData) => {
+		console.log(searchData);
+	}
+
 	return(
 		<>
-		<div className="App">
-		<Search />
+		<div className="container">
+		<Search onSearchChange={handleOnSearchChange} />
 
 
 
--- WeatherForecastApi/src/components/search/search.js
+++ WeatherForecastApi/src/components/search/search.js
@@ -1,4 +1,25 @@
-const Search = () => {
-    return("hello");
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+
+const Search = (onSearchChange) => {
+
+ const [search, setSearch] = useState(null);
+
+ const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+}
+
+
+    return(
+        <AsyncPaginate 
+        placeholder="Search for city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        />
+
+
+    );
 }
 export default Search;
