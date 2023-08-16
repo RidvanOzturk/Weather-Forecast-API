95fc0d66d915ec69202ff8e18a81f675aaa3293e
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-16 19:32:33 +0300

    added  value and label for api cities

--- WeatherForecastApi/src/components/searchbar.jsx
+++ WeatherForecastApi/src/components/searchbar.jsx
@@ -1,43 +1,46 @@
-import React from "react"
-import { useState } from "react"
-import {AsyncPaginate} from "react-select-async-paginate"
-import {geoApiOptions,url} from "../api"
+import React from "react";
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, url } from "../api";
 
-const SearchBar = ({onSearchChange}) => {
-
-  const [search, setSearch] = useState(null)
+const SearchBar = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
 
   const loadOptions = async (inputValue) => {
-    try {
-      const response = await fetch(`${url}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions);
-      const result = await response.text();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+    return fetch(
+      `${url}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name} ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
 
   const handleOnChange = (data) => {
-    setSearch(data)
+    setSearch(data);
     onSearchChange(data);
-
-  }
-
+  };
 
   return (
     <div>
-      
       <AsyncPaginate
-      
         placeholder="Search for city"
         debounceTimeout={600}
         value={search}
         onChange={handleOnChange}
         loadOptions={loadOptions}
       />
-
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
