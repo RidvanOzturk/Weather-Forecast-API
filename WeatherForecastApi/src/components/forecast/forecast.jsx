90dba5fbdb27e493f6644d020f6e473a5b848825
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-28 17:12:50 +0300

    added forecastDays icons and description

--- WeatherForecastApi/src/components/forecast/forecast.jsx
+++ WeatherForecastApi/src/components/forecast/forecast.jsx
@@ -6,7 +6,24 @@ import {
   AccordionItemButton,
 } from "react-accessible-accordion";
 
+const WEEK_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
 const Forecast = ({ data }) => {
+    const dayInAWeek = new Date().getDate();
+const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+  WEEK_DAYS.slice(0, dayInAWeek)
+);
+
+console.log(forecastDays);
+
   return (
     <>
       <label className="title">Daily</label>
@@ -14,9 +31,17 @@ const Forecast = ({ data }) => {
         {data.list.splice(0, 7).map((item, idx) => (
           <AccordionItem key={idx}>
             <AccordionItemHeading>
-                <AccordionItemButton>
-                    hello
-                </AccordionItemButton>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    src={`icons/${item.weather[0].icon}.png`}
+                    alt="weather"
+                    className="icon-small"
+                  />
+                  <label className="day">{forecastDays[idx]}</label>
+                  <label className="day">{item.weather[0].description}</label>
+                </div>
+              </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel></AccordionItemPanel>
           </AccordionItem>
