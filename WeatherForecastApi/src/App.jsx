ed648b5960eec82d6c20d81ac56086d58de18f76
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-28 15:24:58 +0300

    added api key, url and import error will be resolved

--- WeatherForecastApi/src/App.jsx
+++ WeatherForecastApi/src/App.jsx
@@ -1,22 +1,41 @@
-import './App.css';
-import SearchBar from './components/search/searchbar';
-import CurrentWeather from './components/current-weather/current-weather';
-
+import "./App.css";
+import SearchBar from "./components/search/searchbar";
+import CurrentWeather from "./components/current-weather/current-weather";
+import { WEATHER_API_KEY } from "./api";
+import { useState } from "react";
 function App() {
+  const [CurrentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setforecast] = useState(null);
 
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
 
-	const handleOnSearchChange = (searchData) => {
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
 
-	const [lat, lon ] = searchData.value.split(" ");
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
 
-	}
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setforecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
+  }
+  console.log(CurrentWeather);
+  console.log(forecast);
 
-	return(
-		<div className="container">
-			<SearchBar onSearchChange={handleOnSearchChange}/>
-			<CurrentWeather/>
-		</div>
-	)
+  return (
+    <div className="container">
+      <SearchBar onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- WeatherForecastApi/src/api.jsx
+++ WeatherForecastApi/src/api.jsx
@@ -7,3 +7,6 @@ export const geoApiOptions = {
 	}
 };
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "572e94dce7fd9ba5312539818133996d";
+
