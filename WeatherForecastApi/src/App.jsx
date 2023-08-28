d8596fedd69326db848295cce7dd5c04473fc75e
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-28 16:05:11 +0300

    All information has been dynamically rendered.

--- WeatherForecastApi/src/App.jsx
+++ WeatherForecastApi/src/App.jsx
@@ -11,10 +11,10 @@ function App() {
     const [lat, lon] = searchData.value.split(" ");
 
     const currentWeatherFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
     const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
 
     Promise.all([currentWeatherFetch, forecastFetch])
@@ -33,7 +33,7 @@ function App() {
   return (
     <div className="container">
       <SearchBar onSearchChange={handleOnSearchChange} />
-      <CurrentWeather/>
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
